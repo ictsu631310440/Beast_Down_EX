de2fb42349868458ba83bf09127b4e87e55544d4
ictsu631310090 <chanhom_p2@silpakorn.edu>
2023-03-13 23:54:35 +0700

    แก้บัค อนิเมชั้น Boss
    แก้บัคไม่ได้เลย

--- Unity_BeastDown/Assets/Script/enemy/BossAnimation.cs
+++ Unity_BeastDown/Assets/Script/enemy/BossAnimation.cs
@@ -30,16 +30,6 @@ public class BossAnimation : MonoBehaviour
     }
     void Update()
     {
-        if (play_cards.sequenceCardOneToFive[0] == 1 || play_cards.sequenceCardOneToFive[1] == 1 || play_cards.sequenceCardOneToFive[2] == 1
-            || play_cards.sequenceCardOneToFive[3] == 1 || play_cards.sequenceCardOneToFive[4] == 1)
-        {
-            _BossAnimation.SetBool("willAttack", true);
-        }
-        else if (play_cards.sequenceCardOneToFive[0] == 0 && play_cards.sequenceCardOneToFive[1] == 0 && play_cards.sequenceCardOneToFive[2] == 0
-            && play_cards.sequenceCardOneToFive[3] == 0 && play_cards.sequenceCardOneToFive[4] == 0)
-        {
-            _BossAnimation.SetBool("willAttack", false);
-        }
         if (!enemyBoss.F4 || playerDamage.type != 0)
         {
             _BossAnimation.SetInteger("type", enemyBoss.lost_type);
